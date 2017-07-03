8fe76048ffd83d08d14b1ae8ce6fdf5483ad1bdb
Hendrik <drikdoank@gmail.com>
2017-07-03 09:16:59 +0700

    load bikes

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -69,6 +69,15 @@ export class HomePage {
             console.log(this.reviews);
        
           });
+        
+        this.http.get(apiURL+'getproduct', options)
+        .map(res => this.products= res.json())
+        .subscribe(products => {
+            this.products = products['products'];
+            console.log(this.products);
+       
+          });
+
         this.http.get(apiURL+'getbikes', options)
         .map(res => this.bikes= res.json())
         .subscribe(bikes => {
