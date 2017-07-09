a16825def906fd4ca2c538b6785b7016bbcb74fb
Hendrik <drikdoank@gmail.com>
2017-07-10 01:20:39 +0700

    soolve error map

--- src/providers/google-maps/google-maps.ts
+++ src/providers/google-maps/google-maps.ts
@@ -184,26 +184,6 @@ export class GoogleMapsProvider {
 
         // UNCOMMENT IF U WANT USE INFWOWINDOW
         var infowindow = new google.maps.InfoWindow;
-        if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(function(position) {
-            var pos = {
-              lat: position.coords.latitude,
-              lng: position.coords.longitude
-            };
-
-            infoWindow.setPosition(pos);
-            infoWindow.setContent('Location found.');
-            infoWindow.open(map);
-            map.setCenter(pos);
-          }, function() {
-            handleLocationError(true, infoWindow, map.getCenter());
-          });
-        } else {
-          // Browser doesn't support Geolocation
-          handleLocationError(false, infoWindow, map.getCenter());
-        }
-
-
         google.maps.event.addListener(marker, 'click', function() {
            infowindow.setContent(place.name);
            infowindow.open(this.map, this);
