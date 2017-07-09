e6e71043fa30d90fb4744be4435bb4e13cff4304
Hendrik <drikdoank@gmail.com>
2017-07-09 23:54:50 +0700

    soolve error map

--- src/providers/google-maps/google-maps.ts
+++ src/providers/google-maps/google-maps.ts
@@ -136,12 +136,33 @@ export class GoogleMapsProvider {
           var place =[]
           for (var i = 0; i < results.length; i++) {
             place = results[i];
+            var loc1 = latLng
+            if(typeof(place.geometry)!==undefined){
+              var loc2 = place.geometry.location;
+              var dist = loc2.distanceFrom(loc1);
+              results[i].distance=dist;
+              console.log(results[i].distance);
+            }
             createMarker(results[i], map);
           }
           localStorage.setItem('koordinat',JSON.stringify(results));
-
           }
         }
+
+        google.maps.LatLng.prototype.distanceFrom = function(latlng) {
+          var lat = [this.lat(), latlng.lat()]
+          var lng = [this.lng(), latlng.lng()]
+          var R = 6378137;
+          var dLat = (lat[1]-lat[0]) * Math.PI / 180;
+          var dLng = (lng[1]-lng[0]) * Math.PI / 180;
+          var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+          Math.cos(lat[0] * Math.PI / 180 ) * Math.cos(lat[1] * Math.PI / 180 ) *
+          Math.sin(dLng/2) * Math.sin(dLng/2);
+          var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+          var d = R * c;
+          return Math.round(d);
+        }
+
       function createMarker(place, map) {
         var image = {
             url: place.icon,
