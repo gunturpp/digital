c6a61d830858e8394687f6f62a682bd877d1679c
Hendrik <drikdoank@gmail.com>
2017-07-23 18:00:28 +0700

    ganti password

--- src/pages/location-select/location-select.ts
+++ src/pages/location-select/location-select.ts
@@ -44,10 +44,11 @@ export class LocationSelectPage {
         // Filter the items
       
         // Show the results
-        this.showList = true;
+        this.showList = false;
+        this.search(val);
       } else {  
         // hide the results when the query is empty
-        this.showList = false;
+        this.showList = true;
       }
     }
 
@@ -94,6 +95,111 @@ export class LocationSelectPage {
 
     search(place) {
 
+        this.showList = false;
+        this.saveDisabled = true;
+        this.CallLoading();
+        if (place.length > 0 && !this.searchDisabled) {
+            let latLng;
+            this.geolocation.getCurrentPosition().then((position) => {
+                latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+                this.placesService.nearbySearch({
+                    location: latLng,
+                    rankBy: google.maps.places.RankBy.DISTANCE,
+                    name: [place]
+                }, (results, status) => {
+                    callback(results, status, this.maps.map)
+                    this.locations = JSON.parse(localStorage.getItem('koordinat'));
+                    
+                });
+                
+                function clearOverlays(map) {
+                    for (var i = 0; i < map.markers.length; i++ ) {
+                        map.markers[i].setMap(null);
+                    }
+                    map.markers.length = 0;
+                }
+
+                function callback(results, status, map) {
+                    clearOverlays(map);
+                    map.markers= new Array();
+                    if (status == google.maps.places.PlacesServiceStatus.OK) {
+                        for (var i = 0; i <results.length; i++) {
+                            var loc1 = latLng
+                            if (typeof (results[i].geometry) != undefined) {
+                                var loc2 = results[i].geometry.location;
+                                var dist = loc2.distanceFrom(loc1);
+                                results[i].distance = dist;
+                                if(results[i].vicinity != null)
+                                    results[i].alamat = results[i].vicinity.split(',')[0];
+                                else
+                                    results[i].alamat = "";
+                                    
+                                //console.log(results[i].distance);
+
+                            }
+                            createMarker(results[i], map);
+                        }
+                        localStorage.setItem('koordinat', JSON.stringify(results));
+                    }
+                }
+
+
+                google.maps.LatLng.prototype.distanceFrom = function (latlng) {
+                    var lat = [this.lat(), latlng.lat()]
+                    var lng = [this.lng(), latlng.lng()]
+                    var R = 6378137;
+                    var dLat = (lat[1] - lat[0]) * Math.PI / 180;
+                    var dLng = (lng[1] - lng[0]) * Math.PI / 180;
+                    var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+                        Math.cos(lat[0] * Math.PI / 180) * Math.cos(lat[1] * Math.PI / 180) *
+                        Math.sin(dLng / 2) * Math.sin(dLng / 2);
+                    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+                    var d = R * c;
+                    return Math.round(d);
+                }
+
+                function createMarker(place, map) {
+                    var image = {
+                        url: place.icon,
+                        size: new google.maps.Size(71, 71),
+                        origin: new google.maps.Point(0, 0),
+                        anchor: new google.maps.Point(17, 34),
+                        scaledSize: new google.maps.Size(25, 25)
+
+                    }
+                    var placeLoc = place.geometry.location;
+                    var marker = new google.maps.Marker({
+                        map: map,
+                        icon: image,
+                        animation: google.maps.Animation.DROP,
+
+                        position: place.geometry.location,
+                        title: place.name
+                    });
+                    
+                    map.markers.push(marker);
+
+                    // UNCOMMENT IF U WANT USE INFWOWINDOW
+                    var infowindow = new google.maps.InfoWindow;
+                    google.maps.event.addListener(marker, 'click', function () {
+                        infowindow.setContent(place.name);
+                        infowindow.open(this.map, this);
+                    });
+
+                    function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+                        infoWindow.setPosition(pos);
+                        infoWindow.setContent(browserHasGeolocation ?
+                            'Error: The Geolocation service failed.' :
+                            'Error: Your browser doesn\'t support geolocation.');
+                        infoWindow.open(this.map);
+                    }
+                }
+            });
+
+        } else {
+            this.locations = [];
+        }
     }
     
 
